c68aacac1cba12c179bb0f346a55d133a0687f59
Sloke Shrestha <sloke@Slokes-MacBook-Air.local>
2022-02-18 14:39:31 -0600

    Added barebones gunction definition that we need for teensy

--- WaistBand/waistband.ino
+++ WaistBand/waistband.ino
@@ -1,9 +1,32 @@
 #include "classify_data.h"
 
+// length of data in an array
+#define DATA_LENGTH_ACC 9600
+#define DATA_LENGTH_HR 60
+
+#define STRESS_CLASS 1 //if the classification is 1, then this signifies that stress was detected.
+
+void initializeBluetooth();
+void sendAlert();
+void getData();
+
 void setup()
 {
-  
- }
+ initializeBluetooth(); 
+}
 
- void loop(){
+void loop(){
+  double data[DATA_LENGTH_ACC] = getData(); //ONLY USING ACC DATA FOR NOW
+  int class = classifyData(data);
+
+  if (class == STRESS_CLASS){
+    sendAlert();
   }
+
+}
+
+//====================== FUNCITON DEFINITION==================
+void initializeBluetooth(){}
+
+void sendAlert(){}
+void getData(){}
