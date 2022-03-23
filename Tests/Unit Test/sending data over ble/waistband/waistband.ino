96d990bccdb93f032e4954bd538b4c9487f81f0c
Sloke Shrestha <sloke@Slokes-MacBook-Air.local>
2022-03-23 12:49:23 -0500

    original uart pass through code

--- Tests/Unit Test/sending data over ble/waistband/waistband.ino
+++ Tests/Unit Test/sending data over ble/waistband/waistband.ino
@@ -9,12 +9,15 @@ void setup() {
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   Serial.begin(9600);
+  while(!Serial);
   hm10.begin(9600);
-  Serial.println("works");
 }
  
 void loop() {
+  hm10.listen();
+  Serial.println("Listening...");
+  uint8_t c = hm10.read();
+  Serial.println(c);
   Serial.println(hm10.available());
-  Serial.println(hm10.readString());
   delay(1000);
 }
--- Tests/Unit Test/sending data over ble/wristband/wristband.ino
+++ Tests/Unit Test/sending data over ble/wristband/wristband.ino
@@ -18,7 +18,6 @@
 #include <STBLE.h>
 
 
-
 //Debug output adds extra flash and memory requirements!
 #ifndef BLE_DEBUG
 #define BLE_DEBUG true
@@ -36,20 +35,40 @@ uint8_t ble_rx_buffer_len = 0;
 uint8_t ble_connection_state = false;
 #define PIPE_UART_OVER_BTLE_UART_TX_TX 0
 
-
 void setup() {
   SerialMonitorInterface.begin(9600);
-  // while (!SerialMonitorInterface); //This line will block until a serial monitor is opened with TinyScreen+!
+  while (!SerialMonitorInterface); //This line will block until a serial monitor is opened with TinyScreen+!
   BLEsetup();
 }
 
 
 void loop() {
   aci_loop();//Process any ACI commands or events from the NRF8001- main BLE handler, must run often. Keep main loop short.
-  delay(20);//should catch input
-  uint8_t sendBuffer[21] = {'0','1','2','3','4','5','6','7','8','9','0','1','2','3','4','5','6','7','8','9', '\0'};
-  uint8_t sendLength = 21;
-
-  SerialMonitorInterface.println(lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)sendBuffer, sendLength));
+  if (ble_rx_buffer_len) {//Check if data is available
+    SerialMonitorInterface.print(ble_rx_buffer_len);
+    SerialMonitorInterface.print(" : ");
+    SerialMonitorInterface.println((char*)ble_rx_buffer);
+    ble_rx_buffer_len = 0;//clear afer reading
+  }
+  if (SerialMonitorInterface.available()) {//Check if serial input is available to send
+    delay(10);//should catch input
+    uint8_t sendBuffer[21];
+    uint8_t sendLength = 0;
+    while (SerialMonitorInterface.available() && sendLength < 19) {
+      sendBuffer[sendLength] = SerialMonitorInterface.read();
+      sendLength++;
+    }
+    if (SerialMonitorInterface.available()) {
+      SerialMonitorInterface.print(F("Input truncated, dropped: "));
+      if (SerialMonitorInterface.available()) {
+        SerialMonitorInterface.write(SerialMonitorInterface.read());
+      }
+    }
+    sendBuffer[sendLength] = '\0'; //Terminate string
+    sendLength++;
+    if (!lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)sendBuffer, sendLength))
+    {
+      SerialMonitorInterface.println(F("TX dropped!"));
+    }
+  }
 }
-
