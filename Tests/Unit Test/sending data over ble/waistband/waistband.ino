59e037782b94ca3212d6224065b727455993d282
Sloke Shrestha <sloke@Slokes-MacBook-Air.local>
2022-03-28 13:43:57 -0500

    Trying to figure out how to send/receive data over BLE

--- Tests/Unit Test/sending data over ble/waistband/waistband.ino
+++ Tests/Unit Test/sending data over ble/waistband/waistband.ino
@@ -1,23 +1,27 @@
 #include <SoftwareSerial.h>
-#define rxPin 7
-#define txPin 8
-SoftwareSerial hm10(rxPin,txPin); //RX, TX 
+#define rxPinTeensy 7
+#define txPinTeensy 8
 
+// #define rxPinMEGA 15
+// #define txPinMEGA 14
+
+SoftwareSerial hm10(rxPinTeensy,txPinTeensy); //RX, TX 
  
 void setup() {
   // put your setup code here, to run once:
-  pinMode(rxPin, INPUT);
-  pinMode(txPin, OUTPUT);
   Serial.begin(9600);
-  while(!Serial);
   hm10.begin(9600);
 }
  
 void loop() {
-  hm10.listen();
-  Serial.println("Listening...");
-  uint8_t c = hm10.read();
-  Serial.println(c);
-  Serial.println(hm10.available());
-  delay(1000);
+  // put your main code here, to run repeatedly:
+  while(hm10.available()){
+    byte data=hm10.read();
+    Serial.write(data);
+  }
+  while(Serial.available()){
+
+    byte data=Serial.read();
+    hm10.write(data);
+  }
 }
--- Tests/Unit Test/sending data over ble/wristband/wristband.ino
+++ Tests/Unit Test/sending data over ble/wristband/wristband.ino
@@ -18,6 +18,7 @@
 #include <STBLE.h>
 
 
+
 //Debug output adds extra flash and memory requirements!
 #ifndef BLE_DEBUG
 #define BLE_DEBUG true
@@ -30,45 +31,26 @@
 #endif
 
 
-uint8_t ble_rx_buffer[21];
+uint8_t ble_rx_buffer[1];
 uint8_t ble_rx_buffer_len = 0;
 uint8_t ble_connection_state = false;
 #define PIPE_UART_OVER_BTLE_UART_TX_TX 0
 
+
 void setup() {
-  SerialMonitorInterface.begin(9600);
-  while (!SerialMonitorInterface); //This line will block until a serial monitor is opened with TinyScreen+!
+  // while (!SerialMonitorInterface); //This line will block until a serial monitor is opened with TinyScreen+!
   BLEsetup();
 }
 
 
 void loop() {
   aci_loop();//Process any ACI commands or events from the NRF8001- main BLE handler, must run often. Keep main loop short.
-  if (ble_rx_buffer_len) {//Check if data is available
-    SerialMonitorInterface.print(ble_rx_buffer_len);
-    SerialMonitorInterface.print(" : ");
-    SerialMonitorInterface.println((char*)ble_rx_buffer);
-    ble_rx_buffer_len = 0;//clear afer reading
-  }
-  if (SerialMonitorInterface.available()) {//Check if serial input is available to send
-    delay(10);//should catch input
-    uint8_t sendBuffer[21];
-    uint8_t sendLength = 0;
-    while (SerialMonitorInterface.available() && sendLength < 19) {
-      sendBuffer[sendLength] = SerialMonitorInterface.read();
-      sendLength++;
-    }
-    if (SerialMonitorInterface.available()) {
-      SerialMonitorInterface.print(F("Input truncated, dropped: "));
-      if (SerialMonitorInterface.available()) {
-        SerialMonitorInterface.write(SerialMonitorInterface.read());
-      }
-    }
-    sendBuffer[sendLength] = '\0'; //Terminate string
-    sendLength++;
-    if (!lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)sendBuffer, sendLength))
-    {
-      SerialMonitorInterface.println(F("TX dropped!"));
-    }
-  }
+  delay(20);//should catch input
+  uint8_t sendBuffer[1] = {1};
+  uint8_t sendLength = 1;
+  // SerialMonitorInterface.println(sendBuffer[0]);
+  // delay(100);
+lib_aci_send_data(PIPE_UART_OVER_BTLE_UART_TX_TX, (uint8_t*)sendBuffer, sendLength);
 }
+
+
