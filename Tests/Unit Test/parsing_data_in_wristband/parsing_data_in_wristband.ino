8953ce5d127b7130182247cb15ac7866580b962e
Sloke Shrestha <sloke@Slokes-MacBook-Air.local>
2022-04-04 13:41:00 -0500

    Ran the unit test on parsing_data. Test Passed. Results were uploaded

--- Tests/Unit Test/parsing_data_in_wristband/parsing_data_in_wristband.ino
+++ Tests/Unit Test/parsing_data_in_wristband/parsing_data_in_wristband.ino
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+#define TEST true
 #define DEBUG true // flag for serial feedback for debug
 // these lengths are for a minute
 #define DATA_ACC_LENGHT_PER_AXIS 1560 
@@ -43,6 +43,10 @@ void setup(){
     #endif
 }
 void loop(){
+    //test indexes
+    int data_acc_test_idx[] = {1557, 1558, 1559, 3117, 3118, 3119, 4677, 4678, 4679};
+    int data_hr_test_idx[] = {57, 58,59};
+
     generate_data_trivial(ble_buff, loop_counter); //populates the character array
 
     bool data_arr_is_empty = (idx_accx < DATA_ACC_LENGHT_PER_AXIS || idx_accy < DATA_ACC_LENGHT_PER_AXIS*2 || idx_accz < DATA_ACC_LENGHT_PER_AXIS*3 || idx_hr < DATA_HR_LENGTH);
@@ -58,19 +62,52 @@ void loop(){
     
     if(!data_arr_is_empty){ //if data array is populated, run the model, reset data indexes and loop counter
         predict_trivial(data_acc, data_hr);
-        Serial.println(data_acc[4679]);
-        Serial.println(data_hr[59]);
-        delay(10000);
-        reset_idx(idx_accx, idx_accy, idx_accz, idx_hr);
-        Serial.print("idx_accx: ");
-        Serial.println(idx_accx);
-        Serial.print("idx_accy: ");
-        Serial.println(idx_accy);
-        Serial.print("idx_accz: ");
-        Serial.println(idx_accz);
-        Serial.print("idx_hr: ");
+
+        #if TEST
+            Serial.println("=======Test 1: DATA POPULAION TEST=========");
+            // ACC test
+            Serial.print("Contents of data_acc at indexes: [1557, 1558, 1559, 3117, 3118, 3119, 4677, 4678, 4679] are: ");
+            for(int i = 0; i < 3*3; i++){
+                Serial.print(data_acc[data_acc_test_idx[i]]);
+                Serial.print(",");
+            }
+            Serial.println("\n");
+            // HR test
+            Serial.print("The content of data_hr at index [57, 58,59]: ");
+            for(int i = 0; i < 3; i++){
+                Serial.print(data_hr[data_hr_test_idx[i]]);
+                Serial.print(",");
+            }
+
+            Serial.println("\n\n");
+        #endif
+
+        #if TEST
+            Serial.println("=======Test 2: INDEX RESET TEST=========");
+            Serial.println("Before reset_idx is called: ");
+            Serial.print("idx_accx: ");
+            Serial.println(idx_accx);
+            Serial.print("idx_accy: ");
+            Serial.println(idx_accy);
+            Serial.print("idx_accz: ");
+            Serial.println(idx_accz);
+            Serial.print("idx_hr: ");
         Serial.println(idx_hr);
-        delay(10000);
+        #endif
+
+        reset_idx(idx_accx, idx_accy, idx_accz, idx_hr);
+        #if TEST
+            Serial.println("After reset_idx is called: ");
+            Serial.print("idx_accx: ");
+            Serial.println(idx_accx);
+            Serial.print("idx_accy: ");
+            Serial.println(idx_accy);
+            Serial.print("idx_accz: ");
+            Serial.println(idx_accz);
+            Serial.print("idx_hr: ");
+        Serial.println(idx_hr);
+        #endif
+        delay(30000);
         loop_counter = 0;
     }
 }
@@ -170,7 +207,8 @@ void reset_idx(int& accx, int& accy, int& accz, int& hr){
 
 
 void generate_data_trivial(char ble_buff[], int counter){
-    /*Trivial subroutine to simulate data generation to get around bluetooth*/
+    /*Trivial subroutine to simulate data generation to get around bluetooth. 
+    counter will define which data (among accx,accy,accz,hr) to send*/
 
     switch (counter % 4)
     {
@@ -183,7 +221,7 @@ void generate_data_trivial(char ble_buff[], int counter){
         break;
     case 1:
         ble_buff[0] = 'y';
-        ble_buff[1] = '-';
+        ble_buff[1] = '0';
         ble_buff[2] = '5';
         ble_buff[3] = '0';
         ble_buff[4] = '4';
@@ -197,7 +235,7 @@ void generate_data_trivial(char ble_buff[], int counter){
         break;
     case 3:
         ble_buff[0] = 'h';
-        ble_buff[1] = '-';
+        ble_buff[1] = '0';
         ble_buff[2] = '5';
         ble_buff[3] = '0';
         ble_buff[4] = '4';
